77c956c65992c21b6f5f4fcb263a8adef555163c
Toju Mikie <tojumikie@live.com>
2020-06-11 12:45:45 -0400

    added loops program in C#

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int count = 1; count < 6; ++count)
+            //{
+            //    Console.Out.WriteLine("count is " + count);
+            //}
+
+            //int count = 35;
+
+            //do
+            //{
+            //    Console.Out.WriteLine("Count is " + count);
+            //    ++count;
+            //} while (count < 6);
+
+            int[] arr = { 1, 3, 5 };
+
+            foreach (int i in arr)
+            {
+                Console.Out.WriteLine(i + " ");
+            }
+        }
+    }
+}

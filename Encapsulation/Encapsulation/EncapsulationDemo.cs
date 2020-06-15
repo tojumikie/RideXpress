1bd1865b3d71311aeb378c265dd5c38ee6fb90f9
Toju Mikie <tojumikie@live.com>
2020-06-15 12:38:24 -0400

    still studying .NET and C#

---
+++ Encapsulation/Encapsulation/EncapsulationDemo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation
+{
+    class EncapsulationDemo
+    {
+        public void Main(string[] args)
+        {
+            Robot myRobot = new Robot();
+            myRobot.speed = 10;
+            myRobot.RobotType = "Vacuum";
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation/Robot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Encapsulation
+{
+    class Robot
+    {
+        public int speed;
+        private string robotType;
+        static void Main(string[] args)
+        {
+            
+        }
+        public string RobotType
+        {
+            get { return robotType; }
+            set
+            {
+                if (value == null)
+                {
+                    robotType = "";
+                }
+                else
+                {
+                    robotType = value;
+                }
+            }
+        }
+    }
+}

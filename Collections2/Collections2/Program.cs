cee5d1e5c7cbff7dc621ccf975c18c9001dbcfcb
Toju Mikie <tojumikie@live.com>
2020-06-25 13:30:53 -0400

    learning strings in C#

--- Collections2/Collections2/Program.cs
+++ Collections2/Collections2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Collections2
 {
@@ -6,7 +7,33 @@ namespace Collections2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<String, String> contacts = new Dictionary<string, string>();
+
+            contacts.Add("James", "123-456-7890");
+            contacts.Add("GT", "805-867-5309");
+            contacts.Add("Aron", "703-333-1234");
+
+            if (contacts.ContainsKey("Aron"))
+            {
+                contacts.Remove("Aron");
+            }
+            Console.Out.WriteLine(contacts["James"]);
+            String output;
+            if (contacts.ContainsKey("GT"))
+            {
+                contacts.TryGetValue("GT", out output);
+                if (output != null)
+                {
+                    Console.Out.WriteLine(output);
+                }
+            }
+
+            Console.Out.WriteLine("\n");
+
+            foreach (String value in contacts.Values)
+            {
+                Console.Out.WriteLine(value);
+            }
         }
     }
 }
---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region EmptyOrNull
+            String nullString = null;
+            String emptyString = "";
+
+            Console.Out.WriteLine("nullString is null or empty: " + String.IsNullOrEmpty(nullString));
+            Console.Out.WriteLine("emptyString is null or empty: " + String.IsNullOrEmpty(emptyString));
+            #endregion
+
+            #region Escape Characters
+            Console.Out.WriteLine("Use of the \\\\ escape character: \\");
+            Console.Out.WriteLine("Use of the \\n escape character: \nNew line");
+            Console.Out.WriteLine("Use of the \\t escape character:\t\t\t 3-Tabbed line");
+            Console.Out.WriteLine("\Quotes\" must be \"escaped\" to avoid confusing the CLR");
+            Console.In.ReadLine();
+            #endregion
+        }
+    }
+}

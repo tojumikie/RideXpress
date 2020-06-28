b6e91b4f8a176124bfdc8b0e2393977035b3a261
Toju Mikie <tojumikie@live.com>
2020-06-27 23:36:44 -0400

    Learning exceptions in C#

---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zero = 0;
+            try
+            {
+                int a = 1 / zero;
+            }
+            catch (ArithmeticException e)
+            {
+                Console.Out.WriteLine("You can't divide by zero!");
+                Console.Out.WriteLine(e.StackTrace);
+            }
+            catch (StackOverflowException)
+            {
+
+            }
+            finally
+            {
+                Console.Out.WriteLine("I was going to print this anyway.");
+            }
+            Console.Out.WriteLine("I'm okay now!");
+        }
+    }
+}
---
+++ Exceptions2/Exceptions2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exceptions2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Example 1
+            #endregion
+            methodOne();
+        }
+        static void methodOne()
+        {
+            methodTwo();
+        }
+        static void methodTwo()
+        {
+            throw new Exception();
+        }
+    }
+}
